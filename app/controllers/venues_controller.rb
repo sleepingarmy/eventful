5c6f6e0087f020a6367195796930ca422bd06cf5
Rosemary Thomas <rsm.thomas@gmail.com>
2015-06-12 15:17:29 -0600

    venues controller

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -4,8 +4,37 @@ class VenuesController < ApplicationController
     @venues = Venue.all
   end
 
+  def new
+    @venue = Venue.new(venue_params)
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    if @venue.save
+      redirect_to venues_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @venue = Venue.where(id: params[:id])
+  end
+
+  def update
+    @venue = Venue.where(id: params[:id])
+    if @venue.update
+      redirec_to venues_path
+    else
+      render :edit
+  end
   def show
     @venue= Venue.where(id: params[:id])
   end
-  
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:name, :location)
+  end
 end
