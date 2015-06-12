53d3fd051963639e1eb3656b131a0a96caa98e56
Megan Grisley <megangrisley@gmail.com>
2015-06-12 15:16:53 -0600

    built out the bands_controller

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,11 +1,34 @@
 class BandsController < ApplicationController
+  before_action :find_band, only: [:edit. :update, :show, :destroy]
 
   def index
     @bands = Band.all
   end
 
   def show
-  
+  end
+
+  def edit
+  end
+
+  def create
+    @band = Band.new(band_params)
+    if@band.save
+      redirect_to bands_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def band_params
+    params.require(:band).permit(:name, :genre, :venue_id)
+  end
+
+  def find_band
+    @band = Band.find(params[:id])
   end
 
 end
