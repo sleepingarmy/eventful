2748bd59e190111ddf1adbd51eb04e8c19184ed3
Doyi Lee <doyilee@hotmail.com>
2015-06-12 14:08:10 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    get '/' => 'basea#index'
+    get '/' => 'base#index'
     get '/login' => 'sessions#new'
     post '/login' => 'sessions#create'
   end
